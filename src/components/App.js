b309210f901b7f3a4f2c7b25e966cc778ad9d2f2
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-09-25 12:23:30 +0100

    menu & location display done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import Location from "./Location.js"
+import MenuItem from "./MenuItem.js"
+
+const foodItems = [
+  {
+    id: 1,
+    name: "salad",
+    price: 8
+  },
+  { id: 2,
+    name: "cheeseburger",
+    price: 10
+  },
+  { id: 3,
+    name: "steak",
+    price: 15
+  }
+];
+
+const locationItems = [
+  { id: 1,
+    city: "London",
+    phone: '333-33-33'
+  },
+  { id: 2,
+    city: "New York city",
+    phone: '555-55-55'
+  },
+  { id: 3,
+    city: 'Bali',
+    phone: '222-22-22'
+  }
+];
 
 class App extends React.Component {
   render(){
     return (
-      <div>
-        App will go here
-      </div>
+     <div>
+      <ul>
+        {foodItems.map(item => <MenuItem key={item.id} food={item} />)}
+      </ul>
+      <ul>
+        {locationItems.map(item => <Location key={item.id} location={item} />)}
+      </ul>
+      </div> 
     )
   }
 }
